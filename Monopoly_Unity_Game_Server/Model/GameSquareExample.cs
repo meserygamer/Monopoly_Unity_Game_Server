8a8120180a24a0617ec6411359cd65d40880d595
MeseryGamer <meserygamer@outlook.com>
2024-05-10 22:56:09 +0300

    [add] GameSquareExample

---
+++ Monopoly_Unity_Game_Server/Model/GameSquareExample.cs
@@ -0,0 +1,10 @@
+﻿namespace Monopoly_Unity_Game_Server.Model;
+
+public class GameSquareExample
+{
+    public string Example { get; set; } = "";
+
+    public string Answer { get; set; } = "";
+
+    public uint DefaultTimeForAnswerInSecond { get; set; }
+}
