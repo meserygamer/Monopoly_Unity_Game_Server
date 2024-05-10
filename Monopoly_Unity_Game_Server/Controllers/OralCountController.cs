904ec2db2eb5f704ec47aab0e62df28f28810be3
MeseryGamer <meserygamer@outlook.com>
2024-05-10 22:56:55 +0300

    [Add] OralCountController

---
+++ Monopoly_Unity_Game_Server/Controllers/OralCountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Monopoly_Unity_Game_Server.Model;
+
+namespace Monopoly_Unity_Game_Server.Controllers
+{
+    [Route("OralCount/")]
+    public class OralCountController : Controller
+    {
+        [HttpGet]
+        [Route("ExampleWithSingleAction")]
+        public GameSquareExample GenerateExampleWithSingleAction()
+        {
+            
+            return new GameSquareExample();
+        }
+    }
+}
