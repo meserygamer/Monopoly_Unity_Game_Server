8337bb040253d72b2fb5a1cc687be8b9a8e625f2
MeseryGamer <meserygamer@outlook.com>
2024-05-10 22:36:49 +0300

    Добавьте файлы проекта.

---
+++ Monopoly_Unity_Game_Server/Program.cs
@@ -0,0 +1,38 @@
+
+using Microsoft.OpenApi.Models;
+
+namespace Monopoly_Unity_Game_Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddCors();
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "Monopoly Game Server",
+                    Description = ""
+                });
+            });
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseCors(builder => builder.AllowAnyOrigin());
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
