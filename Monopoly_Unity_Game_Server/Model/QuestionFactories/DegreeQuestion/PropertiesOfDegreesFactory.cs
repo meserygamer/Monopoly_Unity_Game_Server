2544c893f28bce9562bcb5040713029207e4e9ce
MeseryGG <meserygamer@gmail.com>
2024-05-16 15:31:58 +0300

    Генераторы степеней пофикшены

--- Monopoly_Unity_Game_Server/Model/QuestionFactories/DegreeQuestion/PropertiesOfDegreesFactory.cs
+++ Monopoly_Unity_Game_Server/Model/QuestionFactories/DegreeQuestion/PropertiesOfDegreesFactory.cs
@@ -83,7 +83,8 @@
             double degreeBase = _random.Next(1, 6);
             double exponentResult = _random.Next(1, 6);
 
-            double firstExponent = _random.Next(1, 6);
+            List<double> doubles = FindAllDividers(exponentResult);
+            double firstExponent = doubles[_random.Next(0, doubles.Count)];
             double secondExponent = exponentResult / firstExponent;
 
             Example firstPartExample = new ExampleWithTwoArguments(new SimpleNumberAsExample(degreeBase), new SimpleNumberAsExample(firstExponent), ActionType.Exponentiation);
@@ -92,5 +93,27 @@
 
             return new UserExample(Math.Pow(degreeBase, exponentResult), finalExample.ExampleInString());
         }
+
+        private List<double> FindAllDividers(double number)
+        {
+            List<double> dividers = new List<double>();
+            List<double> possibleOst = [0, 0.25d, 0.5d, 0.75d, -0d, -0.25d, -0.5d, -0.75d];
+            for (int i = 1; i * i < Math.Abs(number); i++)
+                if (number % i == 0) dividers.AddRange([i, number / i]);
+            for (int i = 0; i < Math.Abs(number); i++)
+            {
+                if (i != 0 && possibleOst.Contains(number / i % 1d) && !dividers.Contains(i))
+                    dividers.AddRange([i, number / i]);
+                if (number % (i + 0.25d) == 0d || possibleOst.Contains(number / (i + 0.25d) % 1d))
+                    dividers.AddRange([i + 0.25d, number / (i + 0.25d)]);
+                if (number % (i + 0.5d) == 0d || possibleOst.Contains(number / (i + 0.5d) % 1d))
+                    dividers.AddRange([i + 0.5d, number / (i + 0.5d)]);
+                if (number % (i + 0.75d) == 0d || possibleOst.Contains(number / (i + 0.75d) % 1d))
+                    dividers.AddRange([i + 0.75d, number / (i + 0.75d)]);
+            }
+            return dividers;
+        }
     }
+
+    
 }
