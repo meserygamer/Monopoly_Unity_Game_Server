1303d79e4582cec28b55d9efcab08a068ab60192
MeseryGG <meserygamer@gmail.com>
2024-05-16 14:02:14 +0300

    фикс генераторов квадратных уравнений

--- Monopoly_Unity_Game_Server/Model/QuestionFactories/QuadraticEquation/QuadraticEquationWithB0_C0Factory.cs
+++ Monopoly_Unity_Game_Server/Model/QuestionFactories/QuadraticEquation/QuadraticEquationWithB0_C0Factory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Monopoly_Unity_Game_Server.Model.QuestionFactories
 {
@@ -7,10 +8,11 @@ namespace Monopoly_Unity_Game_Server.Model.QuestionFactories
         public QuadraticEquationWithB0_C0Factory(Random random)
         {
             _random = random;
+            _possibleDicriminantValues = FindAllNumbersSquares(30);
         }
 
 
-        private readonly double[] POSSIBLE_DISCRIMINANT_VALUES = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+        private List<int> _possibleDicriminantValues;
 
 
         private Random _random;
@@ -34,26 +36,31 @@ namespace Monopoly_Unity_Game_Server.Model.QuestionFactories
 
         private void GenerateQuadraticEquationArgs_B0(out double aArgument, out double bArgument, out double cArgument)
         {
-            aArgument = 0;
-            while (aArgument == 0)
-                aArgument = _random.Next(-10, 11);
+            double discriminant = _possibleDicriminantValues[_random.Next(1, _possibleDicriminantValues.Count)];
             bArgument = 0;
-            double Discriminant = POSSIBLE_DISCRIMINANT_VALUES[_random.Next(0, POSSIBLE_DISCRIMINANT_VALUES.Length)];
-            cArgument = -Discriminant / (4 * aArgument);
+            double discrimenantSecondPart = -discriminant;
+            double aArgumentMultiplycArgument = Math.Round(discrimenantSecondPart / 4, 3);
+            List<double> listPossibleDividers = FindAllDividers(aArgumentMultiplycArgument);
+            List<double> temporaryList = new List<double>();
+            listPossibleDividers.ForEach(a => temporaryList.Add(a * 2));
+            List<double> temp1 = FindAllDividers(Math.Sqrt(discriminant), temporaryList.ToArray());
+            temp1.Remove(1 * 2);
+            temp1.Remove(0 * 2);
+            aArgument = temp1[_random.Next(0, temp1.Count)] / 2;
+            cArgument = aArgumentMultiplycArgument / aArgument;
         }
 
         private void GenerateQuadraticEquationArgs_C0(out double aArgument, out double bArgument, out double cArgument)
         {
-            aArgument = 0;
-            while (aArgument == 0)
-                aArgument = _random.Next(-10, 11);
+            double discriminant = _possibleDicriminantValues[_random.Next(1, _possibleDicriminantValues.Count)];
+            bArgument = Math.Sqrt(discriminant);
             cArgument = 0;
-            double Discriminant = POSSIBLE_DISCRIMINANT_VALUES[_random.Next(0, POSSIBLE_DISCRIMINANT_VALUES.Length)];
-            bArgument = Math.Sqrt(Discriminant + (4 * aArgument * cArgument));
+            List<double> possibleAarguments = FindAllDividers((int)bArgument);
+            aArgument = possibleAarguments[_random.Next(0, possibleAarguments.Count)];
         }
 
-        private string GenerateQuadraticEquationString(double aArgument, double bArgument, double cArgument) => 
-            aArgument + "x^2 + " + ((bArgument != 0)? bArgument + "x" : "") + ((cArgument != 0) ? " + " + cArgument + " = 0" : " = 0");
+        private string GenerateQuadraticEquationString(double aArgument, double bArgument, double cArgument) =>
+            aArgument + "x^2" + ((bArgument != 0) ? $" + {bArgument}x" : "") + ((cArgument != 0) ? $" + {cArgument}" : "") + " = 0";
 
         private string[] CalculateQuadraticEquationResult(double aArgument, double bArgument, double cArgument)
         {
@@ -71,6 +78,51 @@ namespace Monopoly_Unity_Game_Server.Model.QuestionFactories
                     ((-bArgument - Math.Sqrt(Discimenant)) / (2 * aArgument)).ToString()
                 };
             return result;
+        }
+
+        private List<int> FindAllNumbersSquares(int maxNumber)
+        {
+            List<int> allSquares = new List<int>();
+            for (int i = 0; i < maxNumber; i++)
+                allSquares.Add(i * i);
+            return allSquares;
+        }
+
+        private List<double> FindAllDividers(int number)
+        {
+            List<double> dividers = new List<double>();
+            for (int i = 1; i * i <= Math.Abs(number); i++)
+                if (number % i == 0) dividers.AddRange([i, number / i]);
+            return dividers;
+        }
+
+        private List<double> FindAllDividers(double number)
+        {
+            List<double> dividers = new List<double>();
+            List<double> possibleOst = [0, 0.25d, 0.5d, 0.75d, -0d, -0.25d, -0.5d, -0.75d];
+            for (int i = 1; i * i < Math.Abs(number); i++)
+                if (number % i == 0) dividers.AddRange([i, number / i]);
+            for (int i = 0; i < Math.Abs(number); i++)
+            {
+                if (i != 0 && possibleOst.Contains(number / i % 1d) && !dividers.Contains(i))
+                    dividers.AddRange([i, number / i]);
+                if (number % (i + 0.25d) == 0d || possibleOst.Contains(number / (i + 0.25d) % 1d))
+                    dividers.AddRange([i + 0.25d, number / (i + 0.25d)]);
+                if (number % (i + 0.5d) == 0d || possibleOst.Contains(number / (i + 0.5d) % 1d))
+                    dividers.AddRange([i + 0.5d, number / (i + 0.5d)]);
+                if (number % (i + 0.75d) == 0d || possibleOst.Contains(number / (i + 0.75d) % 1d))
+                    dividers.AddRange([i + 0.75d, number / (i + 0.75d)]);
+            }
+            return dividers;
+        }
+
+        private List<double> FindAllDividers(double number, double[] possibleDividers)
+        {
+            List<double> dividers = new List<double>();
+            List<double> possibleOst = [0, 0.25d, 0.5d, 0.75d, -0d, -0.25d, -0.5d, -0.75d];
+            foreach (var i in possibleDividers)
+                if (number % i == 0 || possibleOst.Contains((number / i) % 1)) dividers.Add(i);
+            return dividers;
         }
     }
 }
